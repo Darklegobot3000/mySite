2f165a84169529fe3bb0ff5f03f56d957bbce93b
Alan Zarza <aez222@lehigh.edu>
2021-01-15 21:46:31 -0800

    contact button working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,4 +6,18 @@ window.onload = function(){
         }
       })
     app.message = 'hi'
+
+    let contactBtn = document.getElementById('contactBtn')
+    contactBtn.addEventListener('click',e=>{
+      console.log('clicked button')
+      window.scrollTo(0,document.body.scrollHeight || document.documentElement.scrollHeight)
+
+      console.log(contactBtn.innerHTML)
+      if (contactBtn.innerHTML==="Contact"){
+        contactBtn.innerHTML = 'Back to Top'
+      }
+      else{
+        contactBtn.innerHTML = 'Contact'
+      }
+    })
 }
